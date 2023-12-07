2505adadd5b035763debfe7371935a473f5e3424
Vlad Zaharia <hey@vlad.gg>
2023-12-07 00:25:40 -0800

    Add configs

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,50 +1,78 @@
-import app from '.'
+import app from ".";
 
-describe('Test webfinger', () => {
-  it('Should return 200 response', async () => {
-    const res = await app.request('https://vlad.gg/.well-known/webfinger?resource=acct:hey@vlad.gg')
-    expect(res.status).toBe(200)
-  })
+jest.mock("../config/config.json", () => ({
+  oauth: {
+    issuer: "https://example.com/oidc/",
+  },
 
-  it('Should return 200 response when using localhost', async () => {
-    const res = await app.request('http://localhost:8787/.well-known/webfinger?resource=acct:hey@vlad.gg')
-    expect(res.status).toBe(200)
-  })
+  domains: {
+    "some-domain.com": {},
+  },
+}));
 
-  it('Should return 400 response when resource is not passed in', async () => {
-    const res = await app.request('http://localhost/.well-known/webfinger')
-    expect(res.status).toBe(400)
-    expect(await res.json()).toEqual({ message: 'Missing resource parameter' })
-  })
+describe("Test webfinger", () => {
+  it("Should return 200 response", async () => {
+    const res = await app.request(
+      "https://some-domain.com/.well-known/webfinger?resource=acct:hey@some-domain.com",
+    );
+    expect(res.status).toBe(200);
+  });
 
-  it('Should return 400 response when resource is malformed', async () => {
-    const res = await app.request('http://localhost/.well-known/webfinger?resource=acct:something')
-    expect(res.status).toBe(400)
-    expect(await res.json()).toEqual({ message: 'Resource must be in form acct:email@domain.com' })
-  })
+  it("Should return 200 response when using localhost", async () => {
+    const res = await app.request(
+      "http://localhost:8787/.well-known/webfinger?resource=acct:hey@some-domain.com",
+    );
+    expect(res.status).toBe(200);
+  });
 
-  it('Should return 400 response when domain is not whitelisted', async () => {
-    const res = await app.request('http://localhost/.well-known/webfinger?resource=acct:vlad@example.com')
-    expect(res.status).toBe(400)
-    expect(await res.json()).toEqual({ message: 'Domain is not whitelisted for Polaris Auth access' })
-  })
+  it("Should return 400 response when resource is not passed in", async () => {
+    const res = await app.request("http://localhost/.well-known/webfinger");
+    expect(res.status).toBe(400);
+    expect(await res.json()).toEqual({ message: "Missing resource parameter" });
+  });
 
-  it('Should return 400 response when domain does not match', async () => {
-    const res = await app.request('http://127.0.0.1/.well-known/webfinger?resource=acct:hey@vlad.gg')
-    expect(res.status).toBe(400)
-    expect(await res.json()).toEqual({ message: 'Must access from corresponding domain', url: 'https://vlad.gg/.well-known/webfinger?resource=acct%3Ahey%40vlad.gg' })
-  })
-})
+  it("Should return 400 response when resource is malformed", async () => {
+    const res = await app.request(
+      "http://localhost/.well-known/webfinger?resource=acct:something",
+    );
+    expect(res.status).toBe(400);
+    expect(await res.json()).toEqual({
+      message: "Resource must be in form acct:email@domain.com",
+    });
+  });
 
-describe('Test other routes', () => {
-  it('Main should return 404 response', async () => {
-    const res = await app.request('http://localhost:8787/')
-    expect(res.status).toBe(404)
-  })
+  it("Should return 400 response when domain is not whitelisted", async () => {
+    const res = await app.request(
+      "http://localhost/.well-known/webfinger?resource=acct:example@example.com",
+    );
+    expect(res.status).toBe(400);
+    expect(await res.json()).toEqual({
+      message: "Domain is not whitelisted for access",
+    });
+  });
 
-  it('Other .well-known endpoint should return 404 response', async () => {
-    const res = await app.request('http://localhost:8787/.well-known/somethingelse')
-    expect(res.status).toBe(404)
-  })
-})
+  it("Should return 400 response when domain does not match", async () => {
+    const res = await app.request(
+      "http://127.0.0.1/.well-known/webfinger?resource=acct:hey@some-domain.com",
+    );
+    expect(res.status).toBe(400);
+    expect(await res.json()).toEqual({
+      message: "Must access from corresponding domain",
+      url: "https://some-domain.com/.well-known/webfinger?resource=acct%3Ahey%40some-domain.com",
+    });
+  });
+});
 
+describe("Test other routes", () => {
+  it("Main should return 404 response", async () => {
+    const res = await app.request("http://localhost:8787/");
+    expect(res.status).toBe(404);
+  });
+
+  it("Other .well-known endpoint should return 404 response", async () => {
+    const res = await app.request(
+      "http://localhost:8787/.well-known/somethingelse",
+    );
+    expect(res.status).toBe(404);
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,68 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+import config from "../config/config.json";
 
-const ACCEPTED_DOMAINS = ['polaris.rest', 'vlad.gg', 'vlad.lgbt', 'zhr.one']
+const app = new Hono();
 
-app.get('/.well-known/webfinger', (c) => {
+app.get("/.well-known/webfinger", (c) => {
   // Check if the resource parameter is provided
-  const resource = c.req.query('resource')
+  const resource = c.req.query("resource");
   if (!resource) {
-    return c.json({ message: 'Missing resource parameter' }, 400)
+    return c.json({ message: "Missing resource parameter" }, 400);
   }
 
-  // Check if the domain is whitelisted
-  const domain = resource.split('@')[1]
+  // Check if the domain can be extracted
+  const domain = resource.split("@")[1];
 
   if (!resource.includes("acct:") || !domain) {
-    return c.json({ message: 'Resource must be in form acct:email@domain.com' }, 400)
+    return c.json(
+      { message: "Resource must be in form acct:email@domain.com" },
+      400,
+    );
   }
 
-  if (!ACCEPTED_DOMAINS.includes(domain)) {
-    return c.json({ message: 'Domain is not whitelisted for Polaris Auth access' }, 400)
+  console.log(JSON.stringify(config.domains));
+
+  // Check if the domain is whitelisted
+  if (!config.domains || Object.keys(config.domains).length === 0) {
+    return c.json({ message: "No domains are whitelisted for access" }, 400);
+  }
+
+  if (!Object.keys(config.domains).includes(domain)) {
+    return c.json({ message: "Domain is not whitelisted for access" }, 400);
   }
 
   // Check if the request is coming from the corresponding domain
-  const url = new URL(c.req.url)
-  if (url.hostname !== domain && url.hostname !== 'localhost') {
-    return c.json({ message: `Must access from corresponding domain`, url: `https://${domain}/.well-known/webfinger?resource=${encodeURIComponent(resource)}` }, 400)
+  const url = new URL(c.req.url);
+  if (url.hostname !== domain && url.hostname !== "localhost") {
+    return c.json(
+      {
+        message: `Must access from corresponding domain`,
+        url: `https://${domain}/.well-known/webfinger?resource=${encodeURIComponent(
+          resource,
+        )}`,
+      },
+      400,
+    );
   }
 
   // Set output domain
-  let outputDomain = 'auth.polaris.rest'
-  if (domain === 'zhr.one') {
-    outputDomain = 'auth.zhr.one'
+  let outputDomain = "auth.polaris.rest";
+  if (domain === "zhr.one") {
+    outputDomain = "auth.zhr.one";
   }
 
   // Respond with the webfinger object
   const output = {
-    "subject": resource,
-    "links": [
+    subject: resource,
+    links: [
       {
-        "rel": "http://openid.net/specs/connect/1.0/issuer",
-        "href": `https://${outputDomain}/application/o/tailscale/`
-      }
-    ]
-  }  
-  return c.json(output)
-})
+        rel: "http://openid.net/specs/connect/1.0/issuer",
+        href: `https://${outputDomain}/application/o/tailscale/`,
+      },
+    ],
+  };
+  return c.json(output);
+});
 
-export default app
+export default app;

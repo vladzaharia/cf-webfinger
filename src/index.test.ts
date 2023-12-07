4c4f7baaa139654f75fcdb6d7a15d656e14df640
Vlad Zaharia <hey@vlad.gg>
2023-12-06 23:26:59 -0800

    Initial commit and functionality

---
+++ src/index.test.ts
@@ -0,0 +1,44 @@
+import app from '.'
+
+describe('Test webfinger', () => {
+  it('Should return 200 response', async () => {
+    const res = await app.request('https://vlad.gg/.well-known/webfinger?resource=acct:hey@vlad.gg')
+    expect(res.status).toBe(200)
+  })
+
+  it('Should return 200 response when using localhost', async () => {
+    const res = await app.request('http://localhost:8787/.well-known/webfinger?resource=acct:hey@vlad.gg')
+    expect(res.status).toBe(200)
+  })
+
+  it('Should return 400 response when resource is not passed in', async () => {
+    const res = await app.request('http://localhost/.well-known/webfinger')
+    expect(res.status).toBe(400)
+    expect(await res.json()).toEqual({ message: 'Missing resource parameter' })
+  })
+
+  it('Should return 400 response when domain is not whitelisted', async () => {
+    const res = await app.request('http://localhost/.well-known/webfinger?resource=acct:vlad@example.com')
+    expect(res.status).toBe(400)
+    expect(await res.json()).toEqual({ message: 'Domain is not whitelisted for Polaris Auth access' })
+  })
+
+  it('Should return 400 response when domain does not match', async () => {
+    const res = await app.request('http://127.0.0.1/.well-known/webfinger?resource=acct:hey@vlad.gg')
+    expect(res.status).toBe(400)
+    expect(await res.json()).toEqual({ message: 'Must access from corresponding domain', url: 'https://vlad.gg/.well-known/webfinger?resource=acct%3Ahey%40vlad.gg' })
+  })
+})
+
+describe('Test other routes', () => {
+  it('Main should return 404 response', async () => {
+    const res = await app.request('http://localhost:8787/')
+    expect(res.status).toBe(404)
+  })
+
+  it('Other .well-known endpoint should return 404 response', async () => {
+    const res = await app.request('http://localhost:8787/.well-known/somethingelse')
+    expect(res.status).toBe(404)
+  })
+})
+
---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+const ACCEPTED_DOMAINS = ['polaris.rest', 'vlad.gg', 'vlad.lgbt', 'zhr.one']
+
+app.get('/.well-known/webfinger', (c) => {
+  // Check if the resource parameter is provided
+  const resource = c.req.query('resource')
+  if (!resource) {
+    return c.json({ message: 'Missing resource parameter' }, 400)
+  }
+
+  // Check if the domain is whitelisted
+  const domain = resource.split('@')[1]
+  if (!ACCEPTED_DOMAINS.includes(domain)) {
+    return c.json({ message: 'Domain is not whitelisted for Polaris Auth access' }, 400)
+  }
+
+  // Check if the request is coming from the corresponding domain
+  const url = new URL(c.req.url)
+  if (url.hostname !== domain && url.hostname !== 'localhost') {
+    return c.json({ message: `Must access from corresponding domain`, url: `https://${domain}/.well-known/webfinger?resource=${encodeURIComponent(resource)}` }, 400)
+  }
+
+  // Set output domain
+  let outputDomain = 'auth.polaris.rest'
+  if (domain === 'zhr.one') {
+    outputDomain = 'auth.zhr.one'
+  }
+
+  // Respond with the webfinger object
+  const output = {
+    "subject": resource,
+    "links": [
+      {
+        "rel": "http://openid.net/specs/connect/1.0/issuer",
+        "href": `https://${outputDomain}/application/o/tailscale/`
+      }
+    ]
+  }  
+  return c.json(output)
+})
+
+export default app
